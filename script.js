b7ac67a9fdbb0079ed58da1d6e41372e1d18985d
Luis Miguel Morillas <morillas@gmail.com>
2022-11-15 09:15:59 +0000

    js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-'use strict';
+
+// valores inciales
+let secretNumber = Math.ceil(Math.random() * 20)
+
+let score = 20
+let highscore = 0
+
+const scoreField = document.querySelector('.score')
+const highscoreField = document.querySelector('.highscore')
+const numberField = document.querySelector('.guess')
+
+console.log(`El campo score tiene el valor ${scoreField}`)
+console.log(`El campo highscore tiene el valor ${highscoreField}`)
+console.log(`El campo numberField tiene el valor ${numberField}`)
